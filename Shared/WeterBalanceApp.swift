e2577038e8461c414f948a935243d893bab31306
Dmitry Pyatin <stepanet@gmail.com>
2020-12-16 00:20:13 +0300

    Initial Commit

---
+++ Shared/WeterBalanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeterBalanceApp.swift
+//  Shared
+//
+//  Created by Пятин Дмитрий on 16.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct WeterBalanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
